5bd9281fdd7edf48e5eceb5ffe4cb74ca8189746
unknown <mahendra488@gmail.com>
2021-05-18 23:54:08 +0530

    new corona msg error1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld
 {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
-   f public static void main(String args[])
+    pussblic static void main(String args[])
     {
         System.out.println("Hello, World!!! be safe ...corona");
     }
