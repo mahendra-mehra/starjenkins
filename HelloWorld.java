e61fcea9c2185d945b1edc33b9b7f272506baa92
unknown <mahendra488@gmail.com>
2021-05-19 01:48:24 +0530

    new corona msg pipeline

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ class HelloWorld
 {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
-    pussblic static void main(String args[])
+    public static void main(String args[])
     {
-        System.out.printlnd("Hello, Worasdsadadld!!! be safe ...corona");
+        System.out.println("Hello, Worasdsadadld!!! be safe ...corona");
     }
 }
